ef0fbc447d30ff7533d0e3867bc2cecf0a6bd285
Eden Iyanda <edeniyanda@gmail.com>
2024-08-09 09:26:59 +0100

    UPdate: Added scale for the graphing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import turtle as t
 import math
 
 screen = t.Screen()
-# screen.bgcolor("white")
+# screen.bgcolor("green")
 screen.title("Projectile Motion Simulation")
 
 
@@ -11,6 +11,8 @@ scale = 2
 projectile = t.Turtle()
 projectile.shape("circle")
 projectile.color("blue")
+projectile.penup()
+projectile.goto(-300, -100)
 
 
 def simulate_projectile(v0, angle, g=9.8):
@@ -28,16 +30,16 @@ def simulate_projectile(v0, angle, g=9.8):
         y = (v0 * math.sin(angle_rad) * t - 0.5 * g * t**2) * scale
         
         # Set the projectile new postiton
-        projectile.goto(x , y )
+        projectile.goto(-300 + x , -100+ y )
         
         # Increment the time by the small interval
         t += dt
     
-    projectile.hideturtle()
+    # projectile.hideturtle()
 
 # Set initial conditions
-u0 = 50  # m/s
-theta = 80      # degrees
+u0 = 70  # m/s
+theta = 60    # degrees
 g = 9.8
 
 R = ((u0 ** 2) * math.sin(math.radians(2 * theta)) / g) * scale
@@ -46,7 +48,7 @@ H = ((u0 ** 2) * (math.sin(math.radians(theta)) ** 2) / (2 * g) ) * scale
 # Set up t speed
 projectile.speed(1)
 projectile.penup()
-projectile.goto(0, 0)
+# projectile.goto(0, 0)
 projectile.pendown()
 
 
@@ -56,6 +58,12 @@ projectile.pendown()
 ball1 = t.Turtle()
 ball2 = t.Turtle()
 
+ball1.penup()
+ball2.penup()
+ball1.goto(-300, -100)
+ball2.goto(-300, -100)
+ball1.pendown()
+ball2.pendown()
 ball1.speed(1)
 ball2.speed(1)
 
@@ -66,14 +74,8 @@ ball2.left(90)
 ball2.forward(H)
 
 
-
-
-
-
-
-
 # Simulate the projectile
 simulate_projectile(u0, theta)
 
 # Keep the window open until closed by the user
-t.done()
+screen.exitonclick()
